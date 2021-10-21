68f50e948c8a5f8116a0f2e081b847a410f21bf5
Patrick O'Hara <patrick.h.o-hara@warwick.ac.uk>
2021-10-21 11:26:55 +0100

    ⬆️ Remove python 3.6

--- setup.py
+++ setup.py
@@ -12,12 +12,11 @@ setup(
     ],
     name="pywmlg",
     packages=find_packages(),
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     license="MIT",
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
